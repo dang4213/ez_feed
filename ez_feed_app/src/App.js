8ab0524134fbef957f9286e1580585c1878c048a
Dan Gay <dang4213@gmail.com>
2018-11-06 15:19:15 -0500

    display a random line chart

--- ez_feed_app/src/App.js
+++ ez_feed_app/src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LineChart from './LineChart';
 
 class App extends Component {
+  createFakeData(){
+    // This function creates data that doesn't look entirely random
+    const data = []
+    for (let x = 0; x <= 30; x++) {
+      const random = Math.random();
+      const temp = data.length > 0 ? data[data.length-1].y : 50;
+      const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
+      data.push({x,y})
+    }
+    return data;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="header">react svg line chart [part 1]</div>
+        <LineChart data={this.createFakeData()} />
       </div>
     );
   }
 }
-
 export default App;
